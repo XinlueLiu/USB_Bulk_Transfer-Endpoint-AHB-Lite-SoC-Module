c4ce0b1feddfe42b836654e8e34c6cb7722f57c6
Xinlue Liu <42654960+XinlueLiu@users.noreply.github.com>
2019-11-25 23:26:36 -0500

    Update flex_stp_sr.sv

--- Code/USB RX Modules/flex_stp_sr.sv
+++ Code/USB RX Modules/flex_stp_sr.sv
@@ -1,40 +1,41 @@
 // $Id: $
 // File name:   flex_stp_sr.sv
-// Created:     9/17/2019
-// Author:      David Evans
+// Created:     9/21/2019
+// Author:      Xinlue LIu
 // Lab Section: 337-02
 // Version:     1.0  Initial Design Entry
-// Description: Flex Serial to Parallel Code
-module flex_stp_sr#(parameter NUM_BITS = 9,parameter SHIFT_MSB = 0)(input wire clk, n_rst, shift_enable, serial_in, output reg [(NUM_BITS -1):0] parallel_out);
+// Description: serial to parallel shift register
 
-reg[(NUM_BITS -1):0] next_data;
-reg[(NUM_BITS -1):0] data;
+module flex_stp_sr
+#(
+	parameter NUM_BITS = 4, //4 bit MSB Stp shift register
+	parameter SHIFT_MSB = 1 //TRUE
+)
 
-always_ff @ (posedge clk, negedge n_rst)
-begin
-   if(n_rst == 1'b0) begin
-     parallel_out <= '1;
-   end
-   else begin
-     parallel_out <= next_data;
-   end
+(
+	input wire clk,
+	input wire n_rst,
+	input wire shift_enable,
+	input wire serial_in,
+	output reg [NUM_BITS - 1: 0] parallel_out
+);
+	reg [NUM_BITS - 1: 0] next_parallel_out;
+always_comb begin: NEXT_STATE_LOGIC
+	next_parallel_out = parallel_out;
+	if(!shift_enable) begin
+		next_parallel_out = parallel_out;
+	end else if (SHIFT_MSB == 1) begin
+		next_parallel_out = {parallel_out[NUM_BITS - 2: 0], serial_in};
+	end else begin
+		next_parallel_out = {serial_in, parallel_out[NUM_BITS - 1:1]};
+	end
 end
 
-always_comb 
-begin
-  next_data = parallel_out;
-  if (shift_enable == 1'b1) begin
-    if(SHIFT_MSB == 1) begin
-       next_data = {next_data[(NUM_BITS - 2):0],serial_in};
-    end
-    else begin
-      next_data = {serial_in,next_data[(NUM_BITS - 1):1]};
-    end
-  end
-  else begin
-     next_data = parallel_out;
-  end
+always_ff @(negedge n_rst, posedge clk) begin
+	if(!n_rst)
+		parallel_out <= '1;
+	else
+		parallel_out <= next_parallel_out;
+	end
 
-end
-
-endmodule
+endmodule 
