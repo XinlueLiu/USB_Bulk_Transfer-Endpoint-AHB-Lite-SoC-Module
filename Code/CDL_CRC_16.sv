23813b510bf46e1b9edeb69a1060ad0f2795a932
Xinlue Liu <42654960+XinlueLiu@users.noreply.github.com>
2019-12-03 02:08:42 -0500

    Update CDL_CRC_16.sv

--- Code/CDL_CRC_16.sv
+++ Code/CDL_CRC_16.sv
@@ -15,39 +15,46 @@ module CDL_CRC_16
 	output reg [15:0] inverted_crc
 );
 
-reg [15:0] next_crc;
-reg [15:0] crc;
+reg [16:0] next_crc;
+reg [16:0] crc;
+wire inv;
 
 always_ff @ (negedge n_rst, posedge clk)
 	begin: REG_LOGIC
 	if (!n_rst) begin
+		//crc <= '1;
 		crc <= 0;
 	end else begin
 		crc <= next_crc;
 	end
 end
 
-
 assign inv = input_data ^ crc[15];
-	
+
 always_comb 
 	begin: CRC_LOGIC
 	next_crc = crc;
 	if (reset_crc == 1) begin
-		next_crc = 16'hFFFF;
-		//next_crc = 0;
+		//next_crc = '1;
+		next_crc = 0;
 	end else begin //x^16 + x^15 + x^2 + 1
 		next_crc[15] = crc[14] ^ inv;
 		next_crc[14:3] = crc[13:2];
 		next_crc[2] = crc[1] ^ inv;
 		next_crc[1] = crc[0];
 		next_crc[0] = inv;
+		/*next_crc[16] = crc[15];
+		next_crc[15] = crc[14] ^ input_data ^ crc[15];
+		next_crc[14:3] = crc[13:2];
+		next_crc[2] = crc[1] ^ input_data ^ crc[15];
+		next_crc[1] = crc[0];
+		next_crc[0] = input_data ^ crc[15];*/
 	end
 end
 
 always_comb
 	begin: Invertion
-	inverted_crc = ~crc;	
-	//inverted_crc = crc;
+	//inverted_crc = ~crc[15:0];	
+	inverted_crc = crc;
 	end
 endmodule 
